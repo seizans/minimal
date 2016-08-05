d917405b27c17864803cdc7feeb74f201ca2f8a8
Seizan Shimazaki <seizans@gmail.com>
2016-08-05 12:12:59 +0900

    View, Static 関連を削除する

--- config/config.exs
+++ config/config.exs
@@ -9,7 +9,7 @@ use Mix.Config
 config :minimal, Minimal.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "/Gt8yOm714lkTdT5ohcu12XTIJPXQU9jyNzom3BuENeCJy0TLhKGyWSOXarKarvi",
-  render_errors: [view: Minimal.ErrorView, accepts: ~w(json)]
+  render_errors: [accepts: ~w(json)]
 
 # Configures Elixir's Logger
 config :logger, :console,
