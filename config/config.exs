fee65444b0030482ba1dd1d2d3d243ca3067f53d
Seizan Shimazaki <seizans@gmail.com>
2016-08-05 12:38:26 +0900

    コメントを削除する

--- config/config.exs
+++ config/config.exs
@@ -1,11 +1,5 @@
-# This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
-#
-# This configuration file is loaded before any dependency and
-# is restricted to this project.
 use Mix.Config
 
-# Configures the endpoint
 config :minimal, Minimal.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "/Gt8yOm714lkTdT5ohcu12XTIJPXQU9jyNzom3BuENeCJy0TLhKGyWSOXarKarvi",
@@ -16,6 +10,4 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
 import_config "#{Mix.env}.exs"
--- web/web.ex
+++ web/web.ex
@@ -1,27 +1,4 @@
 defmodule Minimal.Web do
-  @moduledoc """
-  A module that keeps using definitions for controllers,
-  views and so on.
-
-  This can be used in your application as:
-
-      use Minimal.Web, :controller
-      use Minimal.Web, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below.
-  """
-
-  def model do
-    quote do
-      # Define common model functionality
-    end
-  end
-
   def controller do
     quote do
       use Phoenix.Controller
@@ -30,9 +7,6 @@ defmodule Minimal.Web do
     end
   end
 
-  @doc """
-  When used, dispatch to the appropriate controller/view/etc.
-  """
   defmacro __using__(which) when is_atom(which) do
     apply(__MODULE__, which, [])
   end
