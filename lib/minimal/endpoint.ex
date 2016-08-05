b9391e3b7d2ec38e524b45f7507cd87d5a0ae7b7
Seizan Shimazaki <seizans@gmail.com>
2016-08-05 12:16:26 +0900

    Plug.Session を削除する

--- lib/minimal/endpoint.ex
+++ lib/minimal/endpoint.ex
@@ -17,13 +17,5 @@ defmodule Minimal.Endpoint do
 
   plug Plug.Head
 
-  # The session will be stored in the cookie and signed,
-  # this means its contents can be read but not tampered with.
-  # Set :encryption_salt if you would also like to encrypt it.
-  plug Plug.Session,
-    store: :cookie,
-    key: "_minimal_key",
-    signing_salt: "sBanVZS9"
-
   plug Minimal.Router
 end
